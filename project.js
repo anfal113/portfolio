91073f171144f4be1909d12ee15f0cfeb141a2da
anfal113 <anfalck7113@gmail.com>
2024-12-23 09:38:21 +0530

    updated portfolio

---
+++ project.js
@@ -0,0 +1,25 @@
+function toggleMenu() {
+    const nav = document.querySelector('.nav');
+    nav.classList.toggle('show');
+}
+function sendMail() {
+    var params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value,
+    };
+  
+    const serviceID = "service_ajjoru6";
+    const templateID = "template_v7lyotl";
+  
+    emailjs
+      .send(serviceID, templateID, params)
+      .then((res) => {
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("Your message sent successfully!");
+      })
+      .catch((err) => console.log(err));
+  }
